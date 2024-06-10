fb40b7478c5fa9cac1521aae80f19715cb977d7d
Diego Estiben <dbcaturra@gmail.com>
2024-06-10 12:23:25 -0500

    Remove files from tracking according to .gitignore

--- recording-service/src/main.js
+++
@@ -1,5 +0,0 @@
-// Ejemplo básico de app.js para stt-service
-// Aquí deberías agregar la lógica necesaria para el reconocimiento de voz y otras funcionalidades
-
-console.log('¡El servicio de reconocimiento de voz está en funcionamiento!');
-
